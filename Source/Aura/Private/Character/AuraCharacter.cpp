fb975ca243fcccdc86fd255a0a7cdb50dfdd2d49
Westbrook <504899804@qq.com>
2025-08-06 18:35:48 +0800

    01

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,23 @@
+﻿#include "Character/AuraCharacter.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAuraCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
