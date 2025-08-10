43ca280bc406a5227553f8db0ef0f14df1dac2de
Westbrook <504899804@qq.com>
2025-08-10 15:44:11 +0800

    初见GAS相关

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	//设置服务器尝试更新客户端的频率，当服务器上玩家状态更新时，服务器会向所有客户端发送更新，以便同步到最新版本
+	//所有需要同步的变量都会更新，服务器会尽量达到设定的网络更新频率
+	SetNetUpdateFrequency(100.f);
+}
