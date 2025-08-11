baa6501d0b8e6a6bc0a79c645f68f863c17bcc00
Westbrook <504899804@qq.com>
2025-08-11 20:43:56 +0800

    UI

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -9,7 +9,7 @@ public class Aura : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[]
-			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities" });
+			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTasks" });
 
---
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	//当给Widget设置控制器时，我们会调用该函数
+	void OnWidgetControllerSet();
+};
