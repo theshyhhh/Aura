3ae3e84a1911c2adb0c68fbda0c7683f25cd84e2
Westbrook <504899804@qq.com>
2025-09-19 18:26:30 +0800

    网络序列化

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+
+	void SetIsBlockedHit(const bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+	void SetIsCriticalHit(const bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+
+	//必须重载
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FAuraGameplayEffectContext::StaticStruct();
+	}
+
+	//数据网络序列化
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
