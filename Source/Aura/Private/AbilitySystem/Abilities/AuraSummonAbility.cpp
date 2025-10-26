58955a6390bb7a16c9548f0fe786f32bf3d665db
Westbrook <504899804@qq.com>
2025-10-26 10:28:52 +0800

    召唤：1.计算召唤物生成位置

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+
+#include <Programs/UnrealBuildAccelerator/Core/Public/UbaBase.h>
+
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	const FVector Forward = AvatarActor->GetActorForwardVector();
+	const FVector Location = AvatarActor->GetActorLocation();
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+	const float DeltaSpread = SpawnSpread / NumMinions;
+	TArray<FVector> SpawnLocations;
+	for (int i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		UKismetSystemLibrary::DrawDebugArrow(AvatarActor, Location, Location + Direction * MaxSpawnDistance, 10.f, FLinearColor::Red, 10);
+		const float Distance = FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		const FVector SpawnLocation = Location + Direction * Distance;
+		SpawnLocations.Add(SpawnLocation);
+		UKismetSystemLibrary::DrawDebugSphere(AvatarActor, SpawnLocation, 30, 10, FLinearColor::Green, 10);
+	}
+	return SpawnLocations;
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameplayAbility.h"
+#include "AuraSummonAbility.generated.h"
+
+UCLASS()
+class AURA_API UAuraSummonAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category="Summon")
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summon")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summon")
+	float MinSpawnDistance = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summon")
+	float MaxSpawnDistance = 250.f;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summon")
+	float SpawnSpread = 90.f;
+};
