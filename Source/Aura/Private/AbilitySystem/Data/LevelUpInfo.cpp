94747494397f074400a66c231d9aa2cfa93c343b
Westbrook <504899804@qq.com>
2025-11-19 08:44:29 +0800

    完成等级信息数据资产

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,15 @@
+﻿#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelByXP(const int32 XP) const
+{
+	int32 Level = 1;
+	while (true)
+	{
+		if (LevelUpInfo.Num() < Level)return LevelUpInfo.Num() + 1;
+		if (XP >= LevelUpInfo[Level - 1].RequiredXP)
+		{
+			Level++;
+		}
+		else return Level;
+	}
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 RequiredXP;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	uint8 AttributePointsReward;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	uint8 SpellPointReward;
+};
+
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="LevelUpInfomation")
+	TArray<FAuraLevelUpInfo> LevelUpInfo;
+
+	int32 FindLevelByXP(const int32 XP) const;
+};
